2faf6c41211e01511d8cafee0b34dc01a549214f
Bryce Drennan <github@accounts.brycedrennan.com>
2016-04-11 20:59:45 -0700

    Move packages in prep for pypi.

--- eularian_magnification/examples.py
+++ eularian_magnification/examples.py
@@ -1,4 +1,4 @@
-from eulerian_magnify import eulerian_magnification, show_frequencies
+from eularian_magnification.base import eulerian_magnification
 
 # show_frequencies('media/face.mp4')
 eulerian_magnification('media/face.mp4', image_processing='gaussian', pyramid_levels=3, freq_min=50.0 / 60.0, freq_max=1.0, amplification=50)
